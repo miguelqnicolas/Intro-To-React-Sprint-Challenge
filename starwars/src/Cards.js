bb194afbfcc8d6b3d74fac13f831bc7ff756ccf4
Miguel Nicolas <miguelqnicolas@gmail.com>
2019-11-08 14:04:15 -0500

    minor changes

--- starwars/src/Cards.js
+++ starwars/src/Cards.js
@@ -10,14 +10,14 @@ const Cards = props => {
             <CardImg top width="100%" src="/assets/318x180.svg" alt="image" />
             <CardBody>
                 <CardTitle>{props.person.name}</CardTitle>
-                <CardText>Height: {props.person.height}</CardText>
-                <CardText>Mass: {props.person.mass}</CardText>
+                <CardText>Height: {props.person.height} in.</CardText>
+                <CardText>Mass: {props.person.mass} kg.</CardText>
                 <CardText>Hair Color: {props.person.hair_color}</CardText>
                 <CardText>Skin Color: {props.person.skin_color}</CardText>
                 <CardText>Eye Color: {props.person.eye_color}</CardText>
                 <CardText>Birth Year: {props.person.birth_year}</CardText>
                 <CardText>Gender: {props.person.gender}</CardText>
-                <Button href={props.person.url} target='_blank' color='primary'>More</Button>
+                <Button href={props.person.url} target='_blank' color='danger'>More</Button>
             </CardBody>
         </Card>
     )
